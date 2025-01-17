8d29653ff8ab63f576933c01af982feda8ae18f3
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-01-17 18:09:38 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        String name = "Elmo";
+        System.out.println(name);
     }
 }
