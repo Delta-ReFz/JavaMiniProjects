e172fb1e414621edf71f62f853eab8676d1a2fd8
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-03-30 22:14:46 -0400

    lol

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int lol = 5;
-        String name = "Elmo";
-        System.out.println(name);
+       Addition add = new Addition();
+       int result;
+       result = add
     }
 }
