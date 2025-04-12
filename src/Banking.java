431cc809bb2eed9877a24895da17e5a94b1aa09a
Delta-ReFz <el-mehdi.oukid.1@ens.etsmtl.ca>
2025-04-12 17:14:06 -0400

    balance, withdraw

--- src/Banking.java
+++ src/Banking.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Banking {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         //Étape 1 DECLARE VARIABLES
@@ -12,12 +14,11 @@ public class Banking {
         //Étape6 withdraw()
         //Étape7 EXIT MESSAGE
 
-        Scanner scanner = new Scanner(System.in);
-
-        double balance;
+        double balance = 0;
         boolean isRunning = true;
         int choice;
 
+        while (isRunning) {
         System.out.println("***************");
         System.out.println("BANKING PROGRAM");
         System.out.println("***************");
@@ -26,10 +27,63 @@ public class Banking {
         System.out.println("3. Withdraw");
         System.out.println("4. Exit");
         System.out.println("***************");
-        
 
+        System.out.print("Enter your choice (1-4): ");
+        choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance = balance + deposit();
+                case 3 -> balance = balance - withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID INPUT");
+            }
+        }
 
         scanner.close();
-
     }
+
+    static void showBalance(double balance) {
+        System.out.println("***************");
+        System.out.printf("$%.2f\n",balance);
+    }
+
+    static double deposit() {
+
+        double amount;
+
+        System.out.print("Enter amount to deposit: ");
+        amount = scanner.nextDouble();
+
+        if(amount < 0) {
+            System.out.println("INVALID INPUT");
+            return 0;
+        }
+        else {
+            return amount;
+        }
+    }
+
+    static double withdraw(double balance) {
+
+        double amount2;
+
+        System.out.print("Enter amount to withdraw: ");
+        amount2 = scanner.nextDouble();
+
+        if(amount2 > balance) {
+            System.out.println("Not enough money");
+            return 0;
+        }
+        else if (amount2 < 0) {
+            System.out.println("INVALID INPUT");
+            return 0;
+        }
+        else {
+            return amount2;
+        }
+    }
+
+
+
 }
